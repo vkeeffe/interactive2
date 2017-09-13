0811352e0143c2c2d0c1d4b37e3085204de47771
vkeeffe <vkeeffe@cca.edu>
2017-09-13 12:24:23 -0700

    Presentation.key working

--- assets/indexscript.js
+++ assets/indexscript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 $('a').click(function(e) {
     e.preventDefault();  //stop the browser from following
-    window.location.href = 'uploads/file.doc';
+    window.location.href = 'assets/presentation.key';
 });
 
 });
